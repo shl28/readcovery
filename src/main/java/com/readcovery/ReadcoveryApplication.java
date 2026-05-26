0775705f678b50f949b81569e42038fd007da2ab
shl28 <kklim1218@gmail.com>
2026-05-26 19:55:00 +0900

    feat: 프로젝트 초기 셋업(Spring Boot 4.0.6, MySQL연동)

---
+++ src/main/java/com/readcovery/ReadcoveryApplication.java
@@ -0,0 +1,13 @@
+package com.readcovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadcoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadcoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readcovery/ReadcoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readcovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadcoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
